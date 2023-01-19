f15a8aa18fdcc9e4cb13891369cda984058d17bd
Denys <example@gmail.com>
2023-01-19 21:39:44 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+//alert("Hello");
+//let answer = +prompt("Есть ли вам 18?", "Да")
+//console.log(typeof(answer));
+
+//console.log("arr" + "- object");
+//console.log(4 + " - object");
+
+let iner = 10;
+let decr = 10;
+
+    iner++;
+    decr--;
+    
+    console.log(iner);
+    console.log(decr);
